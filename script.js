26b2ac538d67fe10e0a3f6ed0736b5bc654c1712
yohiouwu <siyef10@gmail.com>
2026-02-02 19:47:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const music = document.getElementById("bgMusic");
 const playPauseBtn = document.getElementById("playPause");
 const bearKiss = document.getElementById("bearKiss");
 const bearKiss2 = document.getElementById("bearKiss2");
-const bearKiss3 = document.getElementById("bearKiss3");
 const rosesContainer = document.getElementById("roses-container");
 
 let noCount = 0;
@@ -111,3 +110,4 @@ function createRose() {
 }
 
 setInterval(createRose, 450);
+
